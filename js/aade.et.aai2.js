d7c743bb23ef9dad47336e171ee8319b0c9a40bd
JinjoGuy <59514927+JinjoGuy@users.noreply.github.com>
2020-11-23 10:57:16 -0300

    Create aade.et.aai2.js

---
+++ js/aade.et.aai2.js
@@ -0,0 +1,248 @@
+/* Javascript library containing equivalence table for AAI2 */
+
+aade.equivalenceTable = {
+	0: {
+		'original': '---',
+		'adapted': '---'
+	},
+	1: {
+		'original': '---',
+		'adapted': '---'
+	},
+	2: {
+		'original': 'Edgeworth',
+		'adapted': 'Spada'
+	},
+	3: {
+		'original': 'Gumshoe',
+		'adapted': 'Gaspar'
+	},
+	4: {
+		'original': 'Kay',
+		'adapted': 'Taís'
+	},
+	5: {
+		'original': 'Ema',
+		'adapted': 'Ema'
+	},
+	6: {
+		'original': 'Franziska',
+		'adapted': 'Franziska'
+	},
+	7: {
+		'original': 'Gregory',
+		'adapted': 'Gregório'
+	},
+	8: {
+		'original': 'Badd',
+		'adapted': 'Durano'
+	},
+	9: {
+		'original': 'Lang',
+		'adapted': 'Lang'
+	},
+	10: {
+		'original': 'Judge',
+		'adapted': 'Juiz'
+	},
+	11: {
+		'original': 'Courtney',
+		'adapted': 'Cortês'
+	},
+	12: {
+		'original': 'Debeste',
+		'adapted': 'Omélio'
+	},
+	13: {
+		'original': 'Ray',
+		'adapted': 'Léo'
+	},
+	14: {
+		'original': 'von Karma',
+		'adapted': 'von Karma'
+	},
+	15: {
+		'original': 'Larry',
+		'adapted': 'Vário'
+	},
+	16: {
+		'original': 'Hart',
+		'adapted': 'Sheila'
+	},
+	17: {
+		'original': 'Missile',
+		'adapted': 'Míssil'
+	},
+	18: {
+		'original': 'Huang',
+		'adapted': 'Wang'
+	},
+	19: {
+		'original': 'Knightley',
+		'adapted': 'Cavalcante'
+	},
+	20: {
+		'original': 'Nicole',
+		'adapted': 'Viviane'
+	},
+	21: {
+		'original': 'de Killer',
+		'adapted': 'Mattar'
+	},
+	22: {
+		'original': 'Doe',
+		'adapted': 'da Silva'
+	},
+	23: {
+		'original': 'Dogen',
+		'adapted': 'Falcão'
+	},
+	24: {
+		'original': 'Simon',
+		'adapted': 'Nico'
+	},
+	25: {
+		'original': 'Elbird',
+		'adapted': 'Celas'
+	},
+	26: {
+		'original': 'Roland',
+		'adapted': 'Ulhoa'
+	},
+	27: {
+		'original': 'Sahwit',
+		'adapted': 'Elvisso'
+	},
+	28: {
+		'original': 'Regina',
+		'adapted': 'Regina'
+	},
+	29: {
+		'original': 'Penny',
+		'adapted': 'Nicole'
+	},
+	30: {
+		'original': 'John',
+		'adapted': 'Artur'
+	},
+	31: {
+		'original': 'Master',
+		'adapted': 'Mestre'
+	},
+	32: {
+		'original': 'Kate',
+		'adapted': 'Cacá'
+	},
+	33: {
+		'original': 'Delicia',
+		'adapted': 'Dulce'
+	},
+	34: {
+		'original': 'Gustavia',
+		'adapted': 'Ostos'
+	},
+	35: {
+		'original': 'Dover',
+		'adapted': 'Geraldo'
+	},
+	36: {
+		'original': 'Powers',
+		'adapted': 'Liam'
+	},
+	37: {
+		'original': 'Young',
+		'adapted': 'Jobim'
+	},
+	38: {
+		'original': 'Blaise',
+		'adapted': 'Brás'
+	},
+	39: {
+		'original': 'Karin',
+		'adapted': 'Carmem'
+	},
+	40: {
+		'original': 'Rook',
+		'adapted': 'Torres'
+	},
+	41: {
+		'original': 'Jill',
+		'adapted': 'Mônica'
+	},
+	42: {
+		'original': 'Cameron',
+		'adapted': 'Camela'
+	},
+	43: {
+		'original': 'Officer',
+		'adapted': 'Policial'
+	},
+	44: {
+		'original': 'Guard',
+		'adapted': 'Guarda'
+	},
+	45: {
+		'original': 'Bailiff',
+		'adapted': 'Oficial'
+	},
+	46: {
+		'original': 'Forensics',
+		'adapted': 'Perícia'
+	},
+	47: {
+		'original': 'Payne',
+		'adapted': 'Dores'
+	},
+	48: {
+		'original': 'Audience',
+		'adapted': 'Plateia'
+	},
+	49: {
+		'original': 'TV Crew',
+		'adapted': 'Equipe'
+	},
+	50: {
+		'original': 'Bodyguard',
+		'adapted': 'Guarda'
+	},
+	51: {
+		'original': 'Prisoner',
+		'adapted': 'Preso'
+	},
+	52: {
+		'original': 'Astique',
+		'adapted': 'Astique'
+	},
+	53: {
+		'original': 'Driver',
+		'adapted': 'Motorista'
+	},
+	54: {
+		'original': 'MIB',
+		'adapted': 'MIB'
+	},
+	55: {
+		'original': 'Man',
+		'adapted': 'Homem'
+	},
+	56: {
+		'original': 'Woman',
+		'adapted': 'Mulher'
+	},
+	57: {
+		'original': 'Announcer',
+		'adapted': 'Apresentador'
+	},
+	58: {
+		'original': 'Reporter',
+		'adapted': 'Repórter'
+	},
+	59: {
+		'original': 'Shopkeeper',
+		'adapted': 'Vendedor'
+	},
+	60: {
+		'original': '???',
+		'adapted': '???'
+	}
+};
